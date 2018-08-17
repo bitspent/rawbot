ddea16c27f6215378cf7dd778ca5b95bbc0838df
hassanjawhar <devhassanjawhar@gmail.com>
2018-08-17 17:20:46 +0300

    Updated

--- contracts/Rawbot.sol
+++ contracts/Rawbot.sol
@@ -46,6 +46,11 @@ contract Rawbot is usingOraclize, StandardToken {
         bool available;
     }
 
+    /**
+        Rawbot has 20,000,000 tokens.
+        4,000,000 are held by the contract for the Rawbot team
+        16,000,000 are circulating
+    */
     constructor() StandardToken(20000000, "Rawbot Test 1", "RWT") public payable {
         _rawbot_team = msg.sender;
         balanceOf[_rawbot_team] = (totalSupply * 1) / 5;
@@ -56,6 +61,11 @@ contract Rawbot is usingOraclize, StandardToken {
         user[msg.sender].allowed_to_exchange += 4000000;
     }
 
+    /**
+        This method is used to receive Ethereum & transfer Raw coins in return to the user
+        It adds them to transaction_exchanges mapping and executes all queued exchanges
+        at the same time to avoid fetching Ethereum price at the same time
+    */
     function() payable public {
         require(ETH_PRICE > 0, "ETH price isn't fetched yet");
         transaction_exchanges[index_starter] = transaction_exchange(msg.sender, msg.value, now);
@@ -68,6 +78,10 @@ contract Rawbot is usingOraclize, StandardToken {
         }
     }
 
+    /**
+        This method is used to exchange Raw and get Ethereum in exchange
+        It requires that the user has exchanged Ethereum to Raw previously
+    */
     function withdraw(uint value) public payable returns (bool) {
         require(getExchangeLeftOf(msg.sender) > 0);
         require(getExchangeLeftOf(msg.sender) >= value);
@@ -80,6 +94,10 @@ contract Rawbot is usingOraclize, StandardToken {
         return true;
     }
 
+    /**
+        This method is used to modify user's balance externally
+        It can only be used by created Merchant contract addresses
+    */
     function modifyBalance(address _address, uint256 amount) external returns (bool) {
         MerchantManager merchantManager = MerchantManager(ContractMerchantManagerAddress);
         require(merchantManager.hasAccess(msg.sender) == true);
@@ -88,6 +106,9 @@ contract Rawbot is usingOraclize, StandardToken {
         return true;
     }
 
+    /**
+       This method is used to fetch the Ethereum price using Oraclize API
+    */
     function fetchEthereumPrice(uint timing) onlyOwner public payable {
         if (oraclize_getPrice("URL") > address(this).balance) {
             emit OraclizeLog("Oraclize query was NOT sent, please add some ETH to cover for the query fee", now);
@@ -106,6 +127,10 @@ contract Rawbot is usingOraclize, StandardToken {
         exchangeAll();
     }
 
+    /**
+        This method is used to execute all queued exchanges at the same time
+        Mapping is used instead of arrays to avoid gas spending
+    */
     function exchangeAll() private {
         require(index_checker < index_starter);
         for (uint i = index_checker; i < index_starter; i++) {
@@ -128,27 +153,46 @@ contract Rawbot is usingOraclize, StandardToken {
         index_checker = index_starter;
     }
 
+    /**
+        This method is used to set the MerchantManager's address
+        It's used to manipulate the modifyBalance function
+    */
     function setContractMerchantManager(address _address) onlyOwner public returns (bool){
         ContractMerchantManagerAddress = _address;
         return true;
     }
 
+    /**
+        This method returns the MerchantManager's address
+    */
     function getContractMerchantManager() public view returns (address) {
         return ContractMerchantManagerAddress;
     }
 
+    /**
+        This method returns the exchange addresses
+    */
     function getAddresses() view public returns (address[]) {
         return exchange_addresses;
     }
 
+    /**
+        This method returns the amount of Raw left to exchange
+    */
     function getExchangeLeftOf(address _address) view public returns (uint256){
         return user[_address].allowed_to_exchange;
     }
 
+    /**
+        This method returns the Ethereum price
+    */
     function getEthereumPrice() public view returns (uint) {
         return ETH_PRICE;
     }
 
+    /**
+        This method returns the balance of a specific address
+    */
     function getBalance(address _address) external view returns (uint256){
         return balanceOf[_address];
     }
